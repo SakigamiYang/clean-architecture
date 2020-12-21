7cb6e6c86945ccfb7f70390c269b9d2e349b175b
Sakigami Yang <sakigami@163.com>
2020-12-21 23:07:07 +0800

    update

--- core/domainmodel.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-from abc import ABC
-
-
-class DomainModel(ABC):
-    def __init__(self, id_: str) -> None:
-        self.id_ = id_
--- usecase/Item_request.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-from core import request as req
-
-
-class ItemListRequest(req.ABCRequest):
-    def __init__(self, filters=None) -> None:
-        super().__init__()
-        self.filters = filters
